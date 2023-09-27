81cf025008a3954c3b5659a9cd1d3aca48f2cb00
masir <9ramazan7@mail.ru>
2023-09-28 00:00:35 +0300

    My 3 dz

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("\t\t\t Добро пожаловать в конвертер температуры");
+        System.out.print( "C" + 451+ " = F"+(451*1.80+32.0));
+
+        System.out.println ("\t\t\t Добро пожаловать в конвектор валют");
+        System.out.println("Р"+96+"=$"+0.010*96.30+"=E"+0.0097*94);
+    }
+}
